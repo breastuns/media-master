bfae1fb90dd099fd19971f200baef38060527fc1
Brea Stuns <breastuns@gmail.com>
2019-12-03 23:15:09 -0700

    Add time counters, update styling, add MediaControlSlider Component

--- src/components/MediaControls/MediaControls.js
+++ src/components/MediaControls/MediaControls.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import audioFile from '../../assets/all-star.mp3';
 import { FaPlay, FaPause } from 'react-icons/fa';
+import { MediaControlSlider } from '../MediaControlSlider/MediaControlSlider';
 import './MediaControls.scss';
 
 export class MediaControls extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      duration: '',
+      currentTime: '00:00',
       playing: false,
       audioPlayer: new Audio(audioFile)
+    };
+
+    this.state.audioPlayer.ontimeupdate = () => {
+      this.setState(state => {
+        return {
+          ...state,
+          currentTime: this.calculateTime(this.state.audioPlayer.currentTime)
+        };
+      });
+    };
+
+    this.state.audioPlayer.ondurationchange = () => {
+      this.setState(state => {
+        return {
+          ...state,
+          duration: this.calculateTime(this.state.audioPlayer.duration)
+        };
+      });
     };
 
     this.playAudio = this.playAudio.bind(this);
     this.pauseAudio = this.pauseAudio.bind(this);
   }
+
+  calculateTime = time => {
+    return '0' + Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2);
+  };
 
   togglePlaying = state => {
     return {
@@ -35,11 +60,17 @@ export class MediaControls extends React.Component {
   render() {
     return (
       <div className="media-control-bar">
-        {this.state.playing ? (
-          <FaPause onClick={this.pauseAudio}/>
-        ) : (
-          <FaPlay onClick={this.playAudio}/>
-        )}
+        <div>
+          {this.state.playing ? (
+            <FaPause onClick={this.pauseAudio} />
+          ) : (
+            <FaPlay onClick={this.playAudio} />
+          )}
+        </div>
+        <MediaControlSlider />
+        <div>
+          <span className="time">{`${this.state.currentTime} / ${this.state.duration}`}</span>
+        </div>
       </div>
     );
   }
